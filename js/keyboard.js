836dba5a2cc8746d19bf338a733fc164f99917f0
Anton Salikhmetov <anton.salikhmetov@gmail.com>
2016-08-24 21:53:03 +0300

    Make use of hexogonal computer keyboard layout

--- js/keyboard.js
+++ js/keyboard.js
@@ -3,26 +3,26 @@ var keyboard = (function() {
 
   var module = {};
 
-  var KEYBOARD_NOTES = {
-    /* A */ 65: 0,
-    /* W */ 87: 1,
-    /* S */ 83: 2,
-    /* E */ 69: 3,
-    /* D */ 68: 4,
-    /* F */ 70: 5,
-    /* T */ 84: 6,
-    /* G */ 71: 7,
-    /* Y */ 89: 8,
-    /* Z */ 90: 8,
-    /* H */ 72: 9,
-    /* U */ 85: 10,
-    /* J */ 74: 11,
-    /* K */ 75: 12,
-    /* O */ 79: 13,
-    /* L */ 76: 14,
-  };
+  var map = {};
 
-  var BASE_PITCH = 60;  // middle C
+  var keys = [
+    ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "="],
+    ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]"],
+    ["a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'"],
+    ["z", "x", "c", "v", "b", "n", "m", ",", ".", "/"]
+  ];
+
+  for (let i = 0; i < keys.length; i++) {
+    let row = keys[i];
+
+    for (let j = 0; j < row.length; j++) {
+      let key = row[j];
+
+      map[key] = (4 * i + 7 * j) % 12;
+    }
+  }
+
+  var base = 60;
 
   module.init = function() {
     $(window).keydown(onKeyDown);
@@ -30,21 +30,13 @@ var keyboard = (function() {
   };
 
   var getPitchFromKeyboardEvent = function(event) {
-    var note = BASE_PITCH + KEYBOARD_NOTES[event.which];
+    var note = base + map[event.key];
 
     if (isFinite(note) && !event.ctrlKey && !event.altKey && !event.metaKey)
       return note;
     else
       return null;
   };
-
-  var getChannel = function() {
-    var channel = Number($('#midi-channel').val());
-    if (channel == -1)
-      channel = 0;
-    return channel;
-  };
-
 
   var onKeyDown = function(event) {
     if (somethingHasFocus()) return;
